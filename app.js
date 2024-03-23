49ecf88f5487e8e31926d4cf44570524972d33d0
Hamzaabro5 <hamzaabro683@gmail.com>
2024-03-23 18:08:24 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+
+// Answer No 1  
+
+// const msg = `Hello World`
+// const h2 = document.querySelector(`h2`)
+
+// for (let i = 1; i < 6; i++) {
+//     console.log(msg, i);
+//     h2.innerHTML += `<ol> ${i} - ${msg} </ol>`
+    
+// }
+
+// Answer No 1 
+
+
+
+
+
+
+
+
+// Answer No 2
+
+
+
+
+
+
+
+
+
+
+
+// Answer No 2
+
+
+
+
+
+
+
+
+
