3e3a097e97045d2de0dc379e25ec48f8982a789f
Hamzaabro5 <hamzaabro683@gmail.com>
2024-03-24 17:11:13 +0500

    Second Commit

--- app.js
+++ app.js
@@ -19,6 +19,7 @@
 
 
 
+
 // // Answer No 2
 
 // const h2 = document.querySelector(`h2`)
@@ -26,7 +27,7 @@
 // for (let i = 1; i <= 50; i++) {
 //     console.log(i);
 //     h2.innerHTML += `<ol>${i}</ol>`
-    
+
 // }
 
 
@@ -39,7 +40,20 @@
 
 
 
-// Answer No 3
+
+// // Answer No 3
+
+// const table = prompt(`Which Multipilication Table Do You Want?`)
+// const length = prompt(`How Much Length of Multipilication Table Did You Want?`)
+// const h2 = document.querySelector(`h2`)
+
+// for (let i = 1; i <= length; i++) {
+//     // console.log( `${table} x ${i} = ${table*i}` );
+//     h2.innerHTML += `<ol>${table} x ${i} = ${table*i}</ol>`
+// }
+
+
+// // Answer No 3
 
 
 
@@ -49,4 +63,40 @@
 
 
 
-// Answer No 3
+
+// // Answer No 4
+
+// const array = [`Infinix ` , `Realme` , `Nokia` , `Huawei` , `Samsung` , `Apple` , `Techno`]
+// const h2 = document.querySelector(`h2`)
+
+// for (let i = 0; i < array.length; i++) {
+//    console.log(`${array}`);  
+//     h2.innerHTML += `<li>${array[i]}</li>`
+// }
+
+
+// // Answer No 4
+
+
+
+
+
+
+
+
+
+
+
+// // Answer No 5
+
+// const array = [`Apple` , `Banana` , `Mango` , `Orange` , `Grapes` , `Guava` , `Avacado`]
+// const h2 = document.querySelector(`h2`)
+
+// for (let i = 0; i < 7; i++) {
+
+//     console.log(`Element at index` , [i] , `is` ,[array[i]]  );
+//     h2.innerHTML += `<li>Element at index ${i} is ${array[i]}</li>`
+    
+// }
+
+// // Answer No 5
